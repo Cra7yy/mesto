ec800e3869ddc61349da2ad54d94811950b9ef26
Cra7y <cra7yy@gmail.com>
2022-01-22 14:15:23 +0300

    все замичания по коду исправленны

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,29 @@
 let popupCross = document.querySelector('.pop-up__cross') //кнопка выхода из popup
 let popup = document.querySelector('.pop-up') // popup
 let profileEditor = document.querySelector('.profile__editor') // кнопка вызова popup
-let popupName = document.querySelector('.pop-up__name') // Редактируемое имя в popup
-let popupSign = document.querySelector('.pop-up__sign') // Редактируемая подпись в popup
-let popupSubmit = document.querySelector('.pop-up__submit') // кнопка сохранения в popup
+let popupInputName = document.querySelector('.pop-up__input_name') // Редактируемое имя в popup
+let popupInputSign = document.querySelector('.pop-up__input_sign') // Редактируемая подпись в popup
+let form = document.querySelector('.pop-up__form') // форма в popup
 let profileTitle = document.querySelector('.profile__title') // имя в profile__title
 let profileSubtitle = document.querySelector('.profile__subtitle') //подпись в profile__subtitle
-let gridElementLikes = document.querySelectorAll('.grid-element__like') // иконка сердечка
 
-//Перенос имени и подписи из profile  в popup
-popupName.value = profileTitle.textContent
-popupSign.value = profileSubtitle.textContent
+//функция переноса имени и подписи из profile  в popup
+const transferTextContentPopup = () => {
+  popupInputName.value = profileTitle.textContent
+  popupInputSign.value = profileSubtitle.textContent
+}
+// функция переноса имени и подписи из profile  в popup по клику
+profileEditor.addEventListener('click', transferTextContentPopup)
 
-// функция добавляющая классу pop-up класс popup_open по клику
-profileEditor.addEventListener('click', () => {
+// функция добавляющая классу pop-up класс popup_opened
+const popupOpen = () => {
   popup.classList.add('pop-up_opened')
-})
+}
 
-// функция удаляющая класс popup_open из pop-up
+// функция добавляющая классу pop-up класс popup_opened по клику
+profileEditor.addEventListener('click', popupOpen)
+
+// функция удаляющая класс popup_opened из pop-up
 const close = () => {
   popup.classList.remove('pop-up_opened')
 }
@@ -25,17 +31,15 @@ const close = () => {
 // удаляем класс popup_opened из popup по клику
 popupCross.addEventListener('click', close)
 
-// функция сохраняющая все изменения сделанные в pop-up
-popupSubmit.addEventListener('click', (event) => {
-  event.preventDefault();
-  profileTitle.textContent = popupName.value
-  profileSubtitle.textContent = popupSign.value
-  close()
-})
+// функция переносящая все изменения из pop-up  в profile
+const transferTextContentProfile = () => {
+  profileTitle.textContent = popupInputName.value
+  profileSubtitle.textContent = popupInputSign.value
+}
 
-// переключения вида сердечка с не активного в активное состояние и обратно
-gridElementLikes.forEach((like) => {
-  like.addEventListener('click', () => {
-    like.classList.toggle('grid-element__like_action')
-  })
+// функция сохраняющая все изменения сделанные в pop-up
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+  transferTextContentProfile()
+  close()
 })
